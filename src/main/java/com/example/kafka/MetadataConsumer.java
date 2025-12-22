7a70f7656db6bdf4a4662229f3a245362a9400ef
Abilash Shankarpalli <abhilashabhi0717@gmail.com>
2025-12-22 21:30:37 +0530

    making the consumer as a component

--- src/main/java/com/example/kafka/MetadataConsumer.java
+++ src/main/java/com/example/kafka/MetadataConsumer.java
@@ -7,11 +7,13 @@ import com.example.service.MetadataService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
 
 @RequiredArgsConstructor
 @Slf4j
+@Component
 public class MetadataConsumer {
 
     private MetadataService metadataService;
--- src/main/java/com/example/kafka/MetadataProducer.java
+++ src/main/java/com/example/kafka/MetadataProducer.java
@@ -3,8 +3,10 @@ package com.example.kafka;
 import com.example.model.MetaData;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Component;
 
 @Slf4j
+@Component
 public class MetadataProducer {
 
     private final String presign_topic = "presign-metadata";
